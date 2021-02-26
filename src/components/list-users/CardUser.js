2e79a197fba40ed862ed8696ef6f57f2e27086a0
flowww13 <florent.sauvetage@gmail.com>
2021-02-26 15:43:05 +0100

    style(list-users): delete css file, add JS style

--- src/components/list-users/CardUser.js
+++ src/components/list-users/CardUser.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-import "./../../stylesheets/CardUser.css";
-
 import UserBM from "../../images/bm.jpeg";
 
 export default function CardUser() {
+  const style = {
+    display: "flex",
+    flexDirection: "row",
+  };
+
   return (
-    <div className="card-user">
+    <div style={style}>
       <img src={UserBM} alt={UserBM} />
       <p>Jacques</p>
     </div>
--- src/components/list-users/ListUsers.js
+++ src/components/list-users/ListUsers.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import "./../../stylesheets/ListUsers.css";
+import useWindowSize from "./../../helpers/WindowSize";
 
 import CardUser from "./CardUser";
 
 export default function ListUsers() {
+  useEffect(() => {
+    const getUsers = async () => {
+      console.log("hello");
+    };
+    getUsers();
+  }, []);
+
+  const [width] = useWindowSize();
+
+  const widest = "auto auto auto";
+  const medium = "auto auto";
+  const narrowest = "auto";
+
+  const style = {
+    display: "grid",
+    gridTemplateColumns:
+      width >= 1200 ? widest : width >= 900 ? medium : narrowest,
+    gridGap: "10px",
+    backgroundColor: "#2196F3",
+    padding: "20px",
+    justifyContent: "space-around",
+  };
+
   return (
-    <div className="list-users">
+    <div style={style}>
       <CardUser />
       <CardUser />
       <CardUser />
