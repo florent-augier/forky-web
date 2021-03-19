1769b8577146daee3e293974cfa1c0cce8d536fa
flowww13 <florent.sauvetage@gmail.com>
2021-03-19 09:35:41 +0100

    Feat(CardLunch): add style, connect to back

--- src/components/my-lunches/CardLunch.js
+++ src/components/my-lunches/CardLunch.js
@@ -5,10 +5,15 @@ import {
   mdiCalendarCheckOutline,
   mdiCalendarRemoveOutline,
   mdiCalendarOutline,
+  mdiClockTimeNine,
+  mdiMapMarker,
 } from "@mdi/js";
 import moment from "moment";
+import useWindowSize from "../../helpers/WindowSize";
 
 export default function CardLunch({ lunch }) {
+  const [width] = useWindowSize();
+
   const [color, setColor] = useState(null);
   const [user, setUser] = useState({});
 
@@ -44,20 +49,39 @@ export default function CardLunch({ lunch }) {
 
   const containerCardStyle = {
     display: "flex",
-    justifyContent: "center",
-  };
-
-  const cardStyle = {
-    display: "flex",
-    flexDirection: "row",
-    width: "80%",
-    height: "300px",
+    justifyContent: "space-around",
     backgroundColor: "white",
+  };
+  const gridStyle = {
+    width: "80%",
+    display: "grid",
+    gridGap: "10px",
+    gridAutoRows: "minmax(100px, auto)",
+    border: `1px solid ${color}`,
     borderRadius: "12px",
     boxShadow: "0 2px 4px 0 grey, 0 3px 20px 0 grey",
-    border: `2px solid ${color}`,
-    overflowAnchor: "auto",
   };
+
+  const gridItemStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontFamily: "Poppins-300",
+    margin: "auto",
+    marginTop: "20px",
+  };
+
+  // const cardStyle = {
+  //   display: "flex",
+  //   flexDirection: "row",
+  //   width: "80%",
+  //   height: "300px",
+  //   backgroundColor: "white",
+  //   borderRadius: "12px",
+  //   boxShadow: "0 2px 4px 0 grey, 0 3px 20px 0 grey",
+  //   border: `2px solid ${color}`,
+  //   overflowAnchor: "auto",
+  // };
   const dateStyle = {
     display: "flex",
     justifyContent: "center",
@@ -95,17 +119,114 @@ export default function CardLunch({ lunch }) {
         </span>
       </div>
       <div style={containerCardStyle}>
-        <div style={cardStyle}>
-          <img
-            src={user.photo}
-            alt={user.name}
+        <div style={gridStyle}>
+          <div
             style={{
-              width: "200px",
-              borderRadius: "12px 0 0 12px",
-              borderRight: `1px solid ${color}`,
+              gridColumn: "1",
+              gridRow: "1 / span 2",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
-          ></img>
-          <p>Votre déjeunez avec {user.name ? user.name : ""}</p>
+          >
+            <div
+              style={{
+                borderRadius: "50%",
+                backgroundImage: `url(${user.photo}`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPositionY: "center",
+                border: `2px solid ${color}`,
+
+                width: `${width >= 600 ? "150px" : "80px"}`,
+                height: `${width >= 600 ? "150px" : "80px"}`,
+              }}
+            ></div>
+          </div>
+          <div
+            style={{
+              gridColumn: "2 /span 3",
+              gridRow: "1 / span 2",
+            }}
+          >
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <p
+                style={{
+                  fontFamily: "Poppins-500",
+                  margin: "auto",
+                  marginTop: "20px",
+                  fontSize: "24px",
+                }}
+              >
+                Votre déjeuner avec {user.name}
+              </p>
+              <div
+                style={{ display: "grid", gridTemplateColumns: "auto auto" }}
+              >
+                <div style={gridItemStyle}>
+                  <Icon path={mdiClockTimeNine} size={1} />
+                  <p style={{ marginLeft: "12px" }}>
+                    Rendez-vous:{" "}
+                    <em style={{ color: color }}>
+                      <b>
+                        {lunch.heure === 12
+                          ? "12H"
+                          : lunch.heure === 12.5
+                          ? "12H30"
+                          : lunch.heure === 13
+                          ? "13h"
+                          : "13H30"}
+                      </b>
+                    </em>
+                  </p>
+                </div>
+                <div style={gridItemStyle}>
+                  <Icon path={mdiMapMarker} size={1} />
+
+                  <p style={{ marginLeft: "12px" }}>
+                    Restaurant:{" "}
+                    <em>
+                      <b>{lunch.lieu_propose}</b>
+                    </em>
+                  </p>
+                </div>
+                <div style={gridItemStyle}>
+                  <p style={{ marginLeft: "12px" }}>
+                    Statut de l'invitation:{" "}
+                    <em>
+                      <b>{lunch.statut_invit}</b>
+                    </em>
+                  </p>
+                </div>
+                <div style={gridItemStyle}>
+                  <p style={{ marginLeft: "12px" }}>
+                    Durée:{" "}
+                    <em>
+                      <b>
+                        {lunch.temps_propose === 0.5
+                          ? "30 minutes"
+                          : lunch.temps_propose === 1
+                          ? "1 heure"
+                          : lunch.temps_propose === 1.5
+                          ? "1 heure et 30 minutes"
+                          : "2 heures"}
+                      </b>
+                    </em>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              gridColumn: "1 / 5",
+              gridRow: "3",
+              display: "flex",
+            }}
+          >
+            <div style={{ width: "100%" }}>One</div>
+            <div style={{ width: "100%" }}>Two</div>
+          </div>
         </div>
       </div>
     </div>
